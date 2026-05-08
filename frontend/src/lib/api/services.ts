449c2b43e00ba4b2aa02f52ec80f8111f247ee0f
NinhTinh <ninhtinh742004@gmail.com>
2026-05-08 14:14:36 +0700

    Remove app, public, src folders from frontend

--- frontend/src/lib/api/services.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Central export file for all API services
- */
-
-export { AuthService } from './auth.service';
-export { UserService } from './user.service';
-
-// Export types
-export type { User, UpdateUserRequest, ChangePasswordRequest } from './user.service';
